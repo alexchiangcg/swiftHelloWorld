12ef53e26361989d77515e87a84b948c4e3a5f7f
alexChiang <alexchiang.cg@gmail.com>
2023-09-19 16:04:56 +0800

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by 江德宣 on 2023/9/19.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
