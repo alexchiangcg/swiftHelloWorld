62c3cb798fdb96fe74ae7868450455e9104afba2
alexChiang <alexchiang.cg@gmail.com>
2023-09-19 17:02:13 +0800

    [feature] 2個按鈕

--- HelloWorld/ContentView.swift
+++ HelloWorld/ContentView.swift
@@ -6,16 +6,61 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    //    語音合成器
+    let synthesizer = AVSpeechSynthesizer()
+    
+    
     var body: some View {
+        let buttonName:String = "Happy Programming"
+        let buttonName2:String = "Hello swift !"
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button{
+                spack(text:buttonName)
+                
+            }label: {
+                Text(buttonName)
+                    .fontWeight(.bold)
+                    .font(.system(.title,design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.yellow)
+            .cornerRadius(50)
+            
+            
+            Button{
+                spack(text:buttonName2)
+                
+            }label: {
+                Text(buttonName2)
+                    .fontWeight(.bold)
+                    .font(.system(.title,design: .rounded))
+            }
+            .padding()
+            .foregroundColor(.white)
+            .background(Color.purple)
+            .cornerRadius(50)
         }
-        .padding()
+        //        VStack {
+        //            Image(systemName: "globe")
+        //                .imageScale(.large)
+        //                .foregroundColor(.accentColor)
+        //            Text("Stay Hungry. Stay Foolish.")
+        //                .fontWeight(.heavy)
+        //                .font(.system(.title,design: .rounded))
+        //        }
+        //        .padding()
+    }
+    
+    func spack(text:String){
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: "en-GB")
+        synthesizer.speak(utterance)
+        
     }
 }
 
